516e7663a76dd00e97bb648219c0e8c51bf832ff
Austin Rogers <0705769@KELCE-ACIS08-P>
2018-11-12 18:02:20 -0600

    Created past week hours page with a list box showing last weeks hours worked

---
+++ TeamApp1/TeamApp1/PastHoursPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Collections.ObjectModel;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TeamApp1
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PastHoursPage : ContentPage
+    {
+        public ObservableCollection<Hour> Hours { get; set; }
+        public PastHoursPage()
+        {
+            InitializeComponent();
+
+            this.Hours = new ObservableCollection<Hour>();
+            this.Hours.Add(new Hour
+            { Days = "Monday: ", Hours = 8 });
+            this.Hours.Add(new Hour
+            { Days = "Tuesday: ", Hours = 7.5m });
+            this.Hours.Add(new Hour
+            { Days = "Wednesday: ", Hours = 8.5m });
+            this.Hours.Add(new Hour
+            { Days = "Thursday: ", Hours = 8 });
+            this.Hours.Add(new Hour
+            { Days = "Friday: ", Hours = 8 });
+            this.Hours.Add(new Hour
+            { Days = "Hours worked last week: ", Hours = 40 });
+
+            hoursListView.ItemsSource = this.Hours;
+        }
+
+
+        public class Hour
+        {
+
+            public decimal Hours { get; set; }
+            public string Days { get; set; }
+        }
+    }
+}
+
