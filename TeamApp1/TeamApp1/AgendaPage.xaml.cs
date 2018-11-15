2adcc3181b0a2da755945c6c1308834cd74aa8f0
macenvargas <mvargas9517@live.com>
2018-11-14 18:20:15 -0600

    Worked on Agenda page

--- TeamApp1/TeamApp1/AgendaPage.xaml.cs
+++ TeamApp1/TeamApp1/AgendaPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,12 +10,46 @@ using Xamarin.Forms.Xaml;
 
 namespace TeamApp1
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class AgendaPage : ContentPage
-	{
-		public AgendaPage ()
-		{
-			InitializeComponent ();
-		}
+    public partial class AgendaPage : ContentPage
+    {
+        public ObservableCollection<Week> Time { get; set; }
+
+        public AgendaPage()
+        {
+            InitializeComponent();
+
+            this.Time = new ObservableCollection<Week>();
+
+            this.Time.Add(new Week
+            { Day = "- Monday -" });
+            this.Time.Add(new Week
+            { Day = "- Tuesday -" });
+            this.Time.Add(new Week
+            { Day = "- Wednesday -" });
+            this.Time.Add(new Week
+            { Day = "- Thursday -" });
+            this.Time.Add(new Week
+            { Day = "- Friday -" });
+
+            AgendaListView.ItemsSource = this.Time;
+        }
+
+        private void AgendaListView_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            if(e.SelectedItem == null)
+            {
+                return;
+            }
+            Week selectedWeek = e.SelectedItem as Week;
+
+            (sender as ListView).SelectedItem = null;
+
+            DisplayAlert("Hi", "You Tapped " + selectedWeek.Day, "Ok");
+        }
+
+        public class Week
+        {
+            public string Day { get; set; }
+        }
 	}
 }
