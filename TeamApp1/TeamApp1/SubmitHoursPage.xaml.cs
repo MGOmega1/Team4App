ef25a7b0ab8839a83a36d918f8537a2450cc8e8b
Austin <0705769@KELCE-ACIS08-P>
2018-11-14 18:11:40 -0600

    Added SubmitHoursPage with submit hours entry and navigation to the pasthourspage

---
+++ TeamApp1/TeamApp1/SubmitHoursPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TeamApp1
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class SubmitHoursPage : ContentPage
+	{
+		public SubmitHoursPage ()
+		{
+			InitializeComponent ();
+		}
+
+        private void Button_OnClicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new PastHoursPage());
+        }
+    }
+}
