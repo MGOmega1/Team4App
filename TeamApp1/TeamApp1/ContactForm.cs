aa2741ddc66903f57ab0cdbc1433f12a28fa8247
macenvargas <mvargas9517@live.com>
2018-11-26 17:02:42 -0600

    * App.xaml.cs: Merged Agenda and News page
    * MainPage.xaml:
    * NewsPage.xaml:
    * ContactForm.cs:
    * TeamApp1.csproj:
    * ContactPage.xaml:
    * MainPage.xaml.cs:
    * NewsPage.xaml.cs:
    * ContactPage.xaml.cs:

---
+++ TeamApp1/TeamApp1/ContactForm.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace TeamApp1
+{
+    public class ContactForm : ContentView
+    {
+        public ContactForm()
+        {
+            Content = new Label { Text = "Hello ContentView" };
+        }
+    }
+}
+
