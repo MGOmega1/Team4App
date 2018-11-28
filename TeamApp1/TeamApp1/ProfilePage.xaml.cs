b39468ccdeafb22032479aefb27c97268834d892
0735767 <mgriffin@gus.pittstate.edu>
2018-11-28 17:42:45 -0600

    Entered code behind for profile page

--- TeamApp1/TeamApp1/ProfilePage.xaml.cs
+++ TeamApp1/TeamApp1/ProfilePage.xaml.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Collections.ObjectModel;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -12,14 +12,25 @@ namespace TeamApp1
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class ProfilePage : ContentPage
 	{
-		public ProfilePage ()
+		public ObservableCollection<Profile> employeeProfile { get; set; }
+        public ProfilePage()
 		{
 			InitializeComponent ();
-		}
+            this.employeeProfile = new ObservableCollection<Profile>();
+            this.employeeProfile.Add(new Profile { employeeName = "This Guy", employeeDepartment="That Department", employeeID = "0123456" });
+            this.ProfileListView.ItemsSource = this.employeeProfile;
+
+        }
 
         private void ProfileListView_ItemSelected(object sender, SelectedItemChangedEventArgs e)
         {
 
         }
+    }
+    public class Profile
+    {
+        public string employeeName { get; set; }
+        public string employeeDepartment { get; set; }
+        public string employeeID { get; set; }
     }
 }
