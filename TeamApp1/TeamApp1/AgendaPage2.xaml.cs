9043b68f634b5c6d8a18503fe62d5b8604800102
macenvargas <mvargas9517@live.com>
2018-11-28 18:04:41 -0600

    * AgendaPage1.xaml: Added new page
    * App.xaml.cs:
    * TeamApp1.csproj:
    * AgendaPage2.xaml:
    * AgendaPage2.xaml.cs:

---
+++ TeamApp1/TeamApp1/AgendaPage2.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace TeamApp1
+{
+    public partial class AgendaPage2 : ContentPage
+    {
+        public AgendaPage2()
+        {
+            InitializeComponent();
+
+            Day.Text = DateTime.Today.ToString("dddd");
+
+            if (DateTime.Today.ToString("dddd") == "Monday")
+            {
+                Line1.Text = "10:00pm - Meeting with the CEO";
+                Line2.Text = "12:00pm - Lunch with Client";
+                Line3.Text = "5:00pm - Have Project Finished";
+                Line4.Text = "- New app launch party! Today at 3:30!";
+                Line5.Text = "- Browse new 2019 sustainability goals";
+            }
+            else if (DateTime.Today.ToString("dddd") == "Tuesday")
+            {
+                Line1.Text = "10:00pm - Meeting with the CEO";
+                Line2.Text = "12:00pm - Lunch with Client";
+                Line3.Text = "5:00pm - Have Project Finished";
+                Line4.Text = "- New app launch party! Today at 3:30!";
+                Line5.Text = "- Browse new 2019 sustainability goals";
+            }
+            else if (DateTime.Today.ToString("dddd") == "Wednesday")
+            {
+                Line1.Text = "10:00pm - Meeting with the CEO";
+                Line2.Text = "12:00pm - Lunch with Client";
+                Line3.Text = "5:00pm - Have Project Finished";
+                Line4.Text = "- New app launch party! Today at 3:30!";
+                Line5.Text = "- Browse new 2019 sustainability goals";
+            }
+            else if (DateTime.Today.ToString("dddd") == "Thursday")
+            {
+                Line1.Text = "10:00pm - Meeting with the CEO";
+                Line2.Text = "12:00pm - Lunch with Client";
+                Line3.Text = "5:00pm - Have Project Finished";
+                Line4.Text = "- New app launch party! Today at 3:30!";
+                Line5.Text = "- Browse new 2019 sustainability goals";
+            }
+            else if (DateTime.Today.ToString("dddd") == "Friday")
+            {
+                Line1.Text = "10:00pm - Meeting with the CEO";
+                Line2.Text = "12:00pm - Lunch with Client";
+                Line3.Text = "5:00pm - Have Project Finished";
+                Line4.Text = "- New app launch party! Today at 3:30!";
+                Line5.Text = "- Browse new 2019 sustainability goals";
+            }
+
+        }
+
+        void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- TeamApp1/TeamApp1/App.xaml.cs
+++ TeamApp1/TeamApp1/App.xaml.cs
@@ -11,7 +11,7 @@ namespace TeamApp1
 		{
 			InitializeComponent();
 
-            MainPage = new NavigationPage(new AgendaPage1());
+            MainPage = new NavigationPage(new MainPage());
 		}
 
 		protected override void OnStart ()
